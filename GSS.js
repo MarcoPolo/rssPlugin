a33be6f20e094fc0b885e4079119fc25a5f4484e
Marco Munizaga <marco@h4xasaur.(none)>
2011-07-01 17:33:58 -0400

    added checking

--- GSS.js
+++ GSS.js
@@ -1,41 +1,101 @@
 console.log('hello!!');
 
 var GSS = {}
+//var rssURL = 'http://feeds2.feedburner.com/PitchforkBestNewTracks' 
+var rssURL = 'http://hypem.com/feed/popular/3day/1/feed.xml' 
+var rssURL = 'http://hypem.com/feed/time/today/1/feed.xml'
 GSS.songs = []
+GSS.SongIDs = []
 
 
-$.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&key=ABQIAAAAuIlbOmUd3gJTNVDSvX8ZBBThVXKRlugNJ0FXtFSdeFPX98YKrhQMO67lQJHw2mO0gu2r-chAP3vHeg&q=http://feeds2.feedburner.com/PitchforkBestNewTracks&callback=?', function(resp){
-	console.log(resp);
-	RSS = resp.responseData.feed;
-	buildSearchTerms(RSS);
-})
+
+function getRSS(){
+    $.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&key=ABQIAAAAuIlbOmUd3gJTNVDSvX8ZBBThVXKRlugNJ0FXtFSdeFPX98YKrhQMO67lQJHw2mO0gu2r-chAP3vHeg&q='+rssURL+'&callback=?', function(resp){
+        console.log(resp);
+        RSS = resp.responseData.feed;
+        GSS.rssTitle = RSS.title;
+
+        buildSearchTerms(RSS);
+    })
+}
+
+function makeComparable(name){
+    if(name.indexOf('&amp') != -1){
+        name = name.replace(' &amp','');
+    }
+
+    name = name.replace(/[^a-zA-Z 0-9]+/g,'').toLowerCase();
+
+    return name;
+}
 
 function searchForTerms(searchTerms){
 	searchTerms.map(function(term){
-		GS.service.getSearchResultsEx(term,true, null, function(resp){
-			console.log(term);
-			console.log(resp.result);
-			GSS.songs.push({'title':term, 'results':resp.result});
-		}, null)
+        searchTerm = (term[0]) + ' ' + term[1];
+        GS.service.getSearchResultsEx(searchTerm, true, null, function(resp){
+            console.log(searchTerm);
+            console.log(resp.result);
+            GSS.songs.push({'artist':makeComparable(term[0]), 'songname':term[1], 'results':resp.result, songInfo:''});
+        }, null)
+
+
 
 	});
+
 }
 
 function buildSearchTerms(RSS){
-	RSS.entries.map(function(entry){
+    console.log('starting')
+	var a = RSS.entries.map(function(entry){
+        console.log('innn');
 		searchTerms = [];
-		console.log(entry.title.replace('- ',''));
-		searchTerms.push(entry.title.replace('- ','').replace('"',''));
+		console.log(entry.title.replace('"',''));
+		searchTerms.push(entry.title.replace(/"/g,'').split(' - '));
+
 		searchForTerms(searchTerms);
-	})
+
+        console.log('starting')
+	});
+
+    console.log('done');
+
+    checkResults();
+    
 }
 
 function addRSSToQueue(){
 	GSS.songs.map(function(song){
-		if (typeof song.results[0] != 'undefined'){
-			console.log('adding ', song.title, ' to queue');
-			GS.player.addSongsToQueueAt(song.results[0].SongID,'0',false,'');
-		}
-	});
+            if (typeof song.songInfo.SongID != 'undefined'){
+                console.log('adding ', song.songname, ' to queue');
+                console.log('with song id', song.songInfo.SongID);
+                GS.player.addSongsToQueueAt(song.songInfo.SongID,'0',false,'');
+            }
+    });
 }
 
+function createRSSPlaylist(){
+    var playlistID;
+
+    GS.service.createPlaylist(GSS.title, GSS.SongIDs, 'RSS playlist', function(result, req){playlistID=result},null)
+    console.log(playlistID);
+}
+
+
+function checkResults(){
+    GSS.songs.map(function(song){
+        if(typeof song.results[0] != 'undefined' ){
+            song.results.map(function(result){
+                if((makeComparable(result.ArtistName) == makeComparable(song.artist)) && (makeComparable(result.SongName) == makeComparable(song.songname)) ){
+                    console.log('found the correct result and it is' + result);
+                    song.songInfo = result;
+                }else{
+                    console.error('Did not find ', song.songname, ' by ' , song.artist);
+                }
+            });
+        }
+    });
+
+    GSS.songs.map(function(song) { GSS.SongIDs.push(song.songInfo.SongID) });
+}
+
+getRSS()
